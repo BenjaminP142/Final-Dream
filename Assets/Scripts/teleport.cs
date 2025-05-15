fd5565cb34319b3b3a06357ba619c6700a7ce663
chocapic78560 <matteo.galus@epita.fr>
2025-05-15 17:32:49 +0200

    fix teleport plus jump fini

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class TeleportOnTouch : MonoBehaviour
 {
-
     public Vector3 teleportDestination = new Vector3(10f, 0f, 0f);
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("ggbg"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            transform.position = teleportDestination;
+            other.transform.position = teleportDestination;
             Debug.Log("TeleportOnTouch");
         }
     }
 }
+
 
