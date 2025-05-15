3823eb3744bd9954038ca7a5d3764c0097118714
guettaoren <orenguetta@gmail.com>
2025-05-15 16:58:00 +0200

    milo est la

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -5,9 +5,9 @@ public class TeleportOnTouch : MonoBehaviour
 
     public Vector3 teleportDestination = new Vector3(10f, 0f, 0f);
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("ggbg"))
+        if (other.gameObject.CompareTag("ggbg"))
         {
             transform.position = teleportDestination;
             Debug.Log("TeleportOnTouch");
